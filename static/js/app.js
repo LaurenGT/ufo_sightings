6ddeade05b700177757a03d71d25d57ac4231495
LaurenGT <ltoothaker2@gmail.com>
2021-03-28 11:10:08 -0700

    building out function to apply to event listener

--- static/js/app.js
+++ static/js/app.js
@@ -1,6 +1,9 @@
 // from data.js
 var tableData = data;
 
+// select the filter table button
+let filterTable = d3.select("#filter-btn");
+
 // reference to the table body
 const tbody = d3.select("tbody");
 
@@ -14,3 +17,17 @@ data.forEach(ufoSighting => {
         cell.text(value);
     });
 });
+
+
+// set up function to reference in event listener
+const runEnter = () => {
+    d3.event.preventDefault();
+    let inputElement = d3.select("#datetime");
+    let inputDate = inputElement.property("value");
+    d3.select("tbody").text(inputDate);
+    inputElement.property("value", "");
+}
+
+// apply event listener to the button for click and submit to run function above
+filterTable.on("click", runEnter);
+filterTable.on("submit", runEnter);
