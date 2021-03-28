d7187e031c3729f769a21f8dd9928c0aa8a52d68
LaurenGT <ltoothaker2@gmail.com>
2021-03-28 10:44:02 -0700

    table loaded to page

--- static/js/app.js
+++ static/js/app.js
@@ -1,4 +1,16 @@
 // from data.js
 var tableData = data;
 
+// reference to the table body
+const tbody = d3.select("tbody");
+
 // YOUR CODE HERE!
+// console.log(data);
+
+data.forEach(ufoSighting => {
+    let row = tbody.append("tr");
+    Object.values(ufoSighting).forEach(value => {
+        let cell = row.append("td");
+        cell.text(value);
+    });
+});
